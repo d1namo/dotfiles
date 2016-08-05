4a2eba8d831fcb9979162ea8d4aa612f76a2f501
duffydack <duffydack73@gmail.com>
2016-08-05 12:22:26 +0100

    initial dotfile commit

--- adblock.sh
+++ adblock.sh
@@ -4,11 +4,11 @@
 # if you want to add or remove domains (whitelist/blacklist) then add the domain, eg: www.bing.com to a file named whitelist or blacklist in same folder as the script.  1 url per line.
 
 destinationIP="192.168.1.3"  # your dnsmasq server
-finaloutlist='host.ads'      # the final finished file
+finaloutlist='/home/dean/host.ads'      # the final finished file
 tempoutlist="tempoutlist.tmp" # temp file gets removed on completion.
 tempout="tempout.tmp"         # same as above
 
-## Grabs the lists from multiple sources.  Remove any you don't want.
+#Grabs the lists from multiple sources.  Remove any you don't want.
 echo "Getting yoyo ad list..." 
 curl -s -d mimetype=plaintext -d hostformat=unixhosts http://pgl.yoyo.org/adservers/serverlist.php? | sort > $tempoutlist
 
@@ -34,9 +34,14 @@ echo "Getting Mother of All Ad Blocks list..."
 curl -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -e http://forum.xda-developers.com/ http://adblock.mahakala.is/ | grep -v "#" | awk '{print $2}' | sort >> $tempoutlist
 
 echo "Removing duplicates, processing whitelist/blacklist, and formatting the list..."
+sleep 1
 cat blacklist >> $tempoutlist
-grep -xvf whitelist $tempoutlist > $tempout
-	cat $tempout | sed $'s/\r$//' | sort -u | sed '/^$/d' | awk -v "IP=$destinationIP" '{sub(/\r$/,""); print IP" "$0}' > $finaloutlist
+sleep 1
+grep -vwf whitelist $tempoutlist > $tempout
+#awk 'NR==FNR{A[$1]; next}!($1 in A)' whitelist $tempoutlist > $tempout
+sleep 1
+
+cat $tempout | sed $'s/\r$//' | sort -u | sed '/^$/d' | awk -v "IP=$destinationIP" '{sub(/\r$/,""); print IP" "$0}' > $finaloutlist
 
 # Removes the temporary lists.
 echo "Removing temporary files.."
@@ -45,4 +50,4 @@ rm $tempout
 
 # Count how many domains/whitelists were added so it can be displayed to the user
 numberOfAdsBlocked=$(cat $finaloutlist | wc -l | sed 's/^[ \t]*//')
-echo "$numberOfAdsBlocked ad domains blocked."
+echo "$numberOfAdsBlocked ad domains bilocked."
